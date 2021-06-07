5b1cd5a37593bc4524f2ad0b676f6d3c0a203d58
Kamil <kamilpier08@gmail.com>
2021-06-07 11:24:18 +0200

    init

---
+++ lib/pages/kitchen/kitchenScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class KitchenScreen extends StatelessWidget {
+  const KitchenScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Container(),
+    );
+  }
+}
